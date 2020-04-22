ff2019404934c8bf6e6f543ba1c2d48d0261a3ae
Irfan Nasim <irfan@sdots.local>
2020-04-22 17:33:19 +0500

    multiple chagnes on document library and other stuff

--- src/app/components/document-library/document-library.component.ts
+++ src/app/components/document-library/document-library.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {RequestsService} from "../../service/requests.service";
 import {WbdUtilService} from "../../service/wbd-util.service";
+import {APIURLConstants} from "../../util/api.url.constants";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'document-library-component',
@@ -8,122 +10,57 @@ import {WbdUtilService} from "../../service/wbd-util.service";
 })
 export class DocumentLibraryComponent implements OnInit {
 
-    cars: any[];
     cols: any[];
-    selectedCars1: any[];
+    documents: any[];
+    selectedDocument: any[];
 
     constructor(private requestsService: RequestsService,
-                public wbdUtilService: WbdUtilService) {
+                public wbdUtilService: WbdUtilService,
+                public router: Router) {
     }
 
     ngOnInit() {
+        if (!window.localStorage.getItem(btoa('access_token'))) {
+            this.wbdUtilService.logout();
+            this.router.navigate(['/login']);
+        }
         this.cols = [
             {field: 'name', header: 'Name'},
             {field: 'updated', header: 'Updated'},
             {field: 'size', header: 'Size'},
             {field: 'owner', header: 'Owner'}
         ];
-        this.loadCars();
+        this.loadAllDocuments();
     }
 
-    getIconByType(fileName: string): string {
-        let type = fileName.substr(fileName.lastIndexOf('.') + 1);
-        if (type === 'pdf') {
-            return "assets/icons/pdf-red.png"
-        } else if (type === 'doc') {
-            return "assets/icons/word-blue.png"
-        } else if (type === 'xls') {
-            return "assets/icons/xls-green.png"
-        } else if (type === 'zip') {
-            return "assets/icons/zip-yellow.png"
+    loadAllDocuments() {
+        this.requestsService.getRequest(
+            APIURLConstants.GET_ALL_DOCUMENTS_OWNER_ID + this.wbdUtilService.userInfo.id)
+            .subscribe(
+                (response: Response) => {
+                    if (response['responseCode'] === 'DOC_SUC_01') {
+                        this.documents = response['responseData'];
+                    }
+                },
+                (error: any) => {
+                });
+    }
+
+    getIconByType(document: any): string {
+        if (document.thumbnailSupported) {
+            return document.thumbnailImgUrl;
         } else {
-            return "assets/icons/xls-l-blue.png"
+            if (document.extension === 'pdf') {
+                return "assets/icons/pdf-red.png"
+            } else if (document.extension === 'doc' || document.extension === 'docx') {
+                return "assets/icons/word-blue.png"
+            } else if (document.extension === 'xls' || document.extension === 'xlsx') {
+                return "assets/icons/xls-green.png"
+            } else if (document.extension === 'zip' || document.extension === '7zip' || document.extension === 'rar') {
+                return "assets/icons/zip-yellow.png"
+            } else {
+                return "assets/icons/xls-l-blue.png"
+            }
         }
     }
-
-    loadCars() {
-        this.cars = [
-            {
-                "id": 1,
-                "name": "Sample Pdf Document.pdf",
-                "updated": '11 Mins Ago',
-                "size": "10 KB",
-                "owner": "Irfan Nasim",
-                "icon": "assets/icons/pdf-red.png"
-            },
-            {
-                "id": 2,
-                "name": "Sample Word Document.doc",
-                "updated": '25 Mins Ago',
-                "size": "20 MB",
-                "owner": "Muhammad Imran",
-                "icon": "assets/icons/word-blue.png"
-            },
-            {
-                "id": 3,
-                "name": "Sample Excel Document.xls",
-                "updated": 'Today',
-                "size": "253.5 MB",
-                "owner": "Abdul Ghaffar",
-                "icon": "assets/icons/xls-green.png"
-            },
-            {
-                "id": 4,
-                "name": "Sample File.fil",
-                "updated": '10-March-2020',
-                "size": "10.54 KB",
-                "owner": "Muhammad Suleman",
-                "icon": "assets/icons/xls-l-blue.png"
-            },
-            {
-                "id": 5,
-                "name": "Sample Zip File.zip",
-                "updated": '25-March-2020',
-                "size": "1.5 GB",
-                "owner": "Shahid Iqbal",
-                "icon": "assets/icons/zip-yellow.png"
-            },
-            {
-                "id": 6,
-                "name": "Sample Pdf Document.pdf",
-                "updated": '11 Mins Ago',
-                "size": "10 KB",
-                "owner": "Irfan Nasim",
-                "icon": "assets/icons/pdf-red.png"
-            },
-            {
-                "id": 7,
-                "name": "Sample Word Document.doc",
-                "updated": '25 Mins Ago',
-                "size": "20 MB",
-                "owner": "Muhammad Imran",
-                "icon": "assets/icons/word-blue.png"
-            },
-            {
-                "id": 8,
-                "name": "Sample Excel Document.xls",
-                "updated": 'Today',
-                "size": "253.5 MB",
-                "owner": "Abdul Ghaffar",
-                "icon": "assets/icons/xls-green.png"
-            },
-            {
-                "id": 9,
-                "name": "Sample File.fil",
-                "updated": '10-March-2020',
-                "size": "10.54 KB",
-                "owner": "Muhammad Suleman",
-                "icon": "assets/icons/xls-l-blue.png"
-            },
-            {
-                "id": 10,
-                "name": "Sample Zip File.zip",
-                "updated": '25-March-2020',
-                "size": "1.5 GB",
-                "owner": "Shahid Iqbal",
-                "icon": "assets/icons/zip-yellow.png"
-            },
-        ]
-    }
-
 }
